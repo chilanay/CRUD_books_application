bd27f598e4c0264ee76c09abe948bfb024ef586f
Chilanay <chilanay.hajisoy@gmail.com>
2023-11-29 17:46:00 +0400

    changes made

--- Main.java
+++ Main.java
@@ -5,12 +5,45 @@ import java.sql.Statement;
 
 public class Main {
 
+    // public static void ConnectDatabase(String dbid, String userid, String passwd)
+    // {
+    // try (Connection conn = DriverManager.getConnection(
+    // "jdbc:oracle:thin:@db.yale.edu:2000:univdb", userid = "root", passwd =
+    // "12345");
+    // Statement stmt = conn.createStatement();) {
+    // } catch (SQLException sqle) {
+    // System.out.println("SQLException : " + sqle);
+    // }
+    // }
+
     public static void ConnectDatabase(String dbid, String userid, String passwd) {
-        try (Connection conn = DriverManager.getConnection(
-                "jdbc:oracle:thin:@db.yale.edu:2000:univdb", userid = "root", passwd = "12345");
-                Statement stmt = conn.createStatement();) {
+        try {
+            // Load the PostgreSQL JDBC driver
+            Class.forName("org.postgresql.Driver");
+
+            // Construct the connection URL
+            String url = "jdbc:postgresql://localhost:5433/" + dbid;
+
+            // Establish the connection
+            try (Connection conn = DriverManager.getConnection(url, userid, passwd);
+                    Statement stmt = conn.createStatement()) {
+                // You can perform database operations here if needed
+                System.out.println("Connected to the database");
+            }
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error loading PostgreSQL JDBC driver: " + e.getMessage());
         } catch (SQLException sqle) {
-            System.out.println("SQLException : " + sqle);
+            System.out.println("SQLException: " + sqle);
         }
     }
+
+    public static void main(String[] args) {
+        // Replace these values with your PostgreSQL database credentials
+        String dbid = "your-database-name";
+        String userid = "pg_read_all_data";
+        String passwd = "your-password";
+
+        ConnectDatabase(dbid, userid, passwd);
+    }
+
 }
